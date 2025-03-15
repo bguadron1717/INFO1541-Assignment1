5cd7d1e0b1bb9ee3977791c5768d505fac378b33
Brian Guadron <brianguadron@Brians-Laptop.local>
2025-03-14 23:46:30 -0400

    Updated the emp to emphourlyEmp for my object as the naming is clearer to me. Then I added another test that validates the annual increase on the salary

--- src/test/java/HourlyEmployeeTest.java
+++ src/test/java/HourlyEmployeeTest.java
@@ -6,20 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HourlyEmployeeTest {
 
 
-    HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+    HourlyEmployee hourlyEmp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
 
-    // Created a test that adds positive hours
-    // Then adds negative hours. It should not increase them per the if check in increaseHours
-    // Then adds another positive hours
+    // Created a test that validate adding/increasing hours
+    // Then validate adding/increasing negative hours. It should not increase them per the if check in increaseHours
+    // Then validate adding more positive hours
     @Test
     public void testIncreaseHours() {
-        emp.increaseHours(4);
-        assertEquals(4, emp.getHoursWorked());
+        hourlyEmp.increaseHours(4);
+        assertEquals(4, hourlyEmp.getHoursWorked());
 
-        emp.increaseHours(-2);
-        assertEquals(4, emp.getHoursWorked());
+        hourlyEmp.increaseHours(-2);
+        assertEquals(4, hourlyEmp.getHoursWorked());
 
-        emp.increaseHours(5);
-        assertEquals(9, emp.getHoursWorked());
+        hourlyEmp.increaseHours(5);
+        assertEquals(9, hourlyEmp.getHoursWorked());
+    }
+
+    // Created a test that validates the increase of a 5%(1.64) raise on the salary 32.85
+    // Added the salary plus the raise to my expected variable
+    // Validated the result are equal.
+    @Test
+    public void testAnnualRaise() {
+        double expectedWageWithRaise = 32.85 + 1.64;
+        hourlyEmp.annualRaise();
+        assertEquals(expectedWageWithRaise, hourlyEmp.getWage());
+
     }
 }
