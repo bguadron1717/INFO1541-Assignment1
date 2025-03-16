7d41192acb6484f252557df773549c8ee888f35b
Brian Guadron <brianguadron@Brians-Laptop.local>
2025-03-16 16:49:11 -0400

    Added the test class CommissionEmployeeTest with the unit test testIncreaseSales. I also added the if check to make sure negative sales cant be added

---
+++ src/test/java/CommissionEmployeeTest.java
@@ -0,0 +1,25 @@
+import EmployeeObjects.CommissionEmployee;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommissionEmployeeTest {
+
+    CommissionEmployee commissionEmp = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
+
+
+    // Created a test that validate adding/increasing sales
+    // Then validate adding/increasing negative sales. It should not increase them per the if check
+    // Then validate adding more positive sales
+    @Test
+    public void testIncreaseSales() {
+        commissionEmp.increaseSales(200);
+        assertEquals(200, commissionEmp.getSales());
+
+        commissionEmp.increaseSales(-100);
+        assertEquals(200, commissionEmp.getSales());
+
+        commissionEmp.increaseSales(400);
+        assertEquals(600, commissionEmp.getSales());
+    }
+}
